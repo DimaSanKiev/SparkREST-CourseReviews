bc32592d07859f6ef0e9a4b8c6cc59aa989d2ce0
DimaSanKiev <dmytro.burdyga@gmail.com>
2016-06-14 13:21:04 +0300

    add dao package and DaoException

---
+++ src/main/java/dao/CourseDao.java
@@ -0,0 +1,12 @@
+package dao;
+
+import exc.DaoException;
+import model.Course;
+
+import java.util.List;
+
+public interface CourseDao {
+    void add(Course course) throws DaoException;
+
+    List<Course> findAll();
+}
---
+++ src/main/java/dao/ReviewDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+import exc.DaoException;
+import model.Review;
+
+import java.util.List;
+
+public interface ReviewDao {
+    void add(Review review) throws DaoException;
+
+    List<Review> findAll();
+
+    List<Review> findByCourseId(int courseId);
+}
